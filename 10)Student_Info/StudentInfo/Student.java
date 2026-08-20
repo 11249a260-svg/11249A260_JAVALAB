5ea8933cb1e6d07846d9201c855ab286595ae6ab
Pabbisetty Hemanth Siva Sai Guptha <226149339+11249a260-svg@users.noreply.github.com>
2026-08-20 02:25:31 -0800

    Add files via upload

---
+++ 10)Student_Info/StudentInfo/Student.java
@@ -0,0 +1,9 @@
+package studentInfo;
+
+public class Student {
+    public void displayInfo() {
+        System.out.println("Student Name: John");
+        System.out.println("Roll No: 101");
+        System.out.println("Course: Computer Science");
+    }
+}
