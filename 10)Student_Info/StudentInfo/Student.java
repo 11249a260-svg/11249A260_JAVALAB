240539303d093b811318854b24ffae3d0000354c
Pabbisetty Hemanth Siva Sai Guptha <226149339+11249a260-svg@users.noreply.github.com>
2026-08-20 02:28:52 -0800

    Delete 10)Student_Info directory

--- 10)Student_Info/StudentInfo/Student.java
+++
@@ -1,9 +0,0 @@
-package studentInfo;
-
-public class Student {
-    public void displayInfo() {
-        System.out.println("Student Name: John");
-        System.out.println("Roll No: 101");
-        System.out.println("Course: Computer Science");
-    }
-}
